f17f02930342588c4d49e54f253179f6bbd6d5b0
sistema-facilitacell <facilitacell2026@gmail.com>
2026-05-20 17:42:38 -0300

    Add files via upload

---
+++ sw_rh.js
@@ -0,0 +1,22 @@
+const CACHE = 'facilita-rh-v1';
+const ASSETS = ['./index_rh_v6_pwa.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
